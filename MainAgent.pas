06f8e87295686aa32450c539cdcd1cf879553274
yushadev0 <yusaguverdik@gmail.com>
2026-08-11 11:37:23 +0300

    Made project into a system tray project. first tests

--- MainAgent.pas
+++ MainAgent.pas
@@ -3,11 +3,17 @@ unit MainAgent;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    PopupMenu1: TPopupMenu;
+    TrayIcon1: TTrayIcon;
+    Durum1: TMenuItem;
+    Durum2: TMenuItem;
+    procedure Durum2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +26,10 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Durum2Click(Sender: TObject);
+begin
+  Application.Terminate;
+end;
 
 end.
